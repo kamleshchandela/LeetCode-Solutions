33507967d7298d0592c31d896b1410c631de617c
Kamlesh <kamleshchandela72@gmail.com>
2026-01-04 13:04:04 +0530

    Add solution: 15-3sum

---
+++ 15-3sum/2026-01-04 13.04.04 - Wrong Answer - runtime NA - memory NA.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    var arr = [] ;
+    var ans = [] ;
+    
+    for(let i = 0 ; i < nums.length ; i++){
+        for(let j = i+1 ; j < nums.length ; j++){
+            for(let k = j+1 ; k < nums.length ; k++){
+                arr = [] ;
+                if(nums[i] + nums[j] + nums[k] == 0){
+                    arr.push(nums[i]) ;
+                    arr.push(nums[j]) ;
+                    arr.push(nums[k]) ;
+                    nums.splice(j , 1) ;
+                    ans.push(arr) ;
+                    
+                }
+                    
+            }
+        }
+    }
+    return ans ;
+};
