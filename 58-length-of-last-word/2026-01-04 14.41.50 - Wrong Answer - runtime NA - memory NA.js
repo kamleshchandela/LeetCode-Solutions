a5db5d6f0de262b0e76e76e6817a80148b1f5ae8
Kamlesh <kamleshchandela72@gmail.com>
2026-01-04 14:41:50 +0530

    Add solution: 58-length-of-last-word

---
+++ 58-length-of-last-word/2026-01-04 14.41.50 - Wrong Answer - runtime NA - memory NA.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    var count = 0 ; 
+    for(let i = s.length - 1  ; i > 0 ; i--){
+        
+        if(s[i] != " "){
+            count ++ ;
+        }
+        else if (count > 0 || s[i] == undefined ){
+            // console.log(count) ;
+            return count ;
+            
+        }
+    }
+};
