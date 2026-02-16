a52558c42ed30b7591a02361c9fca0c01b4061d2
Kamlesh <kamleshchandela72@gmail.com>
2026-02-16 19:48:14 +0530

    Add solution: 387-first-unique-character-in-a-string

---
+++ 387-first-unique-character-in-a-string/2026-02-16 19.48.14 - Time Limit Exceeded - runtime NA - memory NA.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    if(s.length == 1){
+        return 0 ;
+    }
+    for(let i = 0 ; i < s.length ; i++){
+        var check = true ;
+        for(let j = 0 ; j < s.length ; j++){
+            if(s[i] == s[j] && i != j){
+                check = false ;
+                continue; 
+            }
+        }
+        if(check){
+            return i ;
+        }
+        
+        
+    }
+    return -1 ;
+};
