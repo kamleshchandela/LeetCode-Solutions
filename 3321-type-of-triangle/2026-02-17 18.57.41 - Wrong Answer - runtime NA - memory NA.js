8ae544e289825e48619e0c0dbe288e63e55a6084
Kamlesh <kamleshchandela72@gmail.com>
2026-02-17 18:57:41 +0530

    Add solution: 3321-type-of-triangle

---
+++ 3321-type-of-triangle/2026-02-17 18.57.41 - Wrong Answer - runtime NA - memory NA.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+    if(nums[0] == nums[1] && nums[1] == nums[2]){
+        return "equilateral" ;
+    }
+    else if(nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]){
+        return "isosceles" ;
+    }
+    else if (nums[0] + nums[1] > nums[2] || nums[1] + nums[2] > nums[0] || nums[2] + nums[0] > nums[1]){
+        return "scalene" ;
+    }
+    else{
+        return "none" ;
+    }
+};
