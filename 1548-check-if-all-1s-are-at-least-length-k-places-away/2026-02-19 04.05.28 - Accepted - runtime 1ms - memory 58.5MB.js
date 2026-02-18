505956757d58d45fba927ce1368a92a304cbdb38
Kamlesh <kamleshchandela72@gmail.com>
2026-02-19 04:05:28 +0530

    Add solution: 1548-check-if-all-1s-are-at-least-length-k-places-away

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/2026-02-19 04.05.28 - Accepted - runtime 1ms - memory 58.5MB.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var kLengthApart = function(nums, k) {
+    for(let i = 0 ; i < nums.length ; i++){
+        if(nums[i] == 1){
+            for(let j = i + 1 ; j <= i + k && j < nums.length ; j++){
+                if(nums[j] == 1){
+                    return false ;
+                }
+            }
+        }
+    }
+    return true ;
+};
