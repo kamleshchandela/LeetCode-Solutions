215f592ce53bead94dc6b33c24cbe04c4d837455
Kamlesh <kamleshchandela72@gmail.com>
2026-02-19 17:56:26 +0530

    Add solution: 2254-check-if-every-row-and-column-contains-all-numbers

---
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2026-02-19 17.56.26 - Accepted - runtime 99ms - memory 62.1MB.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var checkValid = function(matrix) {
+    for(let i = 0 ; i < matrix.length ; i++){
+        for(let j = 0 ; j < matrix.length ; j ++){
+            for(let k = 0 ; k < matrix.length ; k++){
+                if(matrix[i][j] == matrix[i][k] && j != k && matrix[i][j] <= matrix.length && matrix[i][k] <= matrix.length){
+                    return false ;
+                }
+            }
+        }
+    }
+
+    for(let i = 0 ; i < matrix.length ; i++){
+        for(let j = 0 ; j < matrix.length ; j++){
+            for(let k = 0 ; k < matrix.length ; k++ ){
+                if(matrix[j][i] == matrix[k][i] && j != k && matrix[j][i] <= matrix.length && matrix[k][i] <= matrix.length){
+                    return false ;
+                }
+            }
+        }
+    }
+
+    return true ;
+};
