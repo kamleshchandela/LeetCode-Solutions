17c759fdf0e462c4c54d2ddc294fbcb69711778c
Kamlesh <kamleshchandela72@gmail.com>
2026-02-19 21:39:51 +0530

    Add solution: 507-perfect-number

---
+++ 507-perfect-number/2026-02-19 21.39.51 - Time Limit Exceeded - runtime NA - memory NA.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+    let sum = 0 ; 
+    for(let i = 0 ; i <= num / 2 ; i++){
+        if(num % i == 0){
+            sum = sum + i ;
+        }
+    }
+    if(sum == num){
+        return true ;
+    }
+    else{
+        return false ;
+    }
+};
