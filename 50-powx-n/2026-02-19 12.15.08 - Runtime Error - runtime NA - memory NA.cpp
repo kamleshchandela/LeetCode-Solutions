23a87a085bba483405365ed410d78d6fe3a3f500
Kamlesh <kamleshchandela72@gmail.com>
2026-02-19 12:15:08 +0530

    Add solution: 50-powx-n

---
+++ 50-powx-n/2026-02-19 12.15.08 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x == 1 || n == 1){
+            return x ;
+        }
+        double mul = 1 ;
+        bool check = true ;
+        if(n < 0){
+            check = false ;
+            n = n * (-1) ;
+        }
+        
+        for(int i = 0 ; i < n ; i++){
+            mul = mul * x ;
+        } 
+
+        if(check){
+            return mul ;
+        }
+        else{
+            return 1 / mul ;
+        }
+           
+        
+
+        return mul ;
+    }
+};
