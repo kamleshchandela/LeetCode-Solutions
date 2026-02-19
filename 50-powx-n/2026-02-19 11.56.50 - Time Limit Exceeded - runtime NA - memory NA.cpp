f9bff4d494b8bb71795a450a6a837a261458124c
Kamlesh <kamleshchandela72@gmail.com>
2026-02-19 11:56:50 +0530

    Add solution: 50-powx-n

---
+++ 50-powx-n/2026-02-19 11.56.50 - Time Limit Exceeded - runtime NA - memory NA.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double mul = 1 ;
+        if(n >= 0){
+           for(int i = 0 ; i < n ; i++){
+                mul = mul * x ;
+            } 
+        }
+        else{
+            n = -n ;
+            for(int i = 0 ; i < n ; i++){
+                mul = mul * x ;
+            } 
+            return 1 / mul ;
+        }
+        
+
+        return mul ;
+    }
+};
