5e1a26de3459e3ab08d2b5aa73e321aa395fb5d9
Kamlesh <kamleshchandela72@gmail.com>
2026-02-20 18:18:42 +0530

    Add solution: 29-divide-two-integers

---
+++ 29-divide-two-integers/2026-02-20 18.18.42 - Accepted - runtime 1ms - memory 56.5MB.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function(dividend, divisor) {
+    const INT_MAX = 2**31 - 1;
+    const INT_MIN = -(2**31);
+    if(dividend == INT_MIN && divisor == -1) return INT_MAX;
+    if(dividend == INT_MIN && divisor == 1) return INT_MIN;
+    return parseInt(dividend / divisor) ;
+};
