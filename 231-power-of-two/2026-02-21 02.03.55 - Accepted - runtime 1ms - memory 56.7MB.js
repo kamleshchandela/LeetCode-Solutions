9753517a047167e258d56ed242e759725fb9f6f8
Kamlesh <kamleshchandela72@gmail.com>
2026-02-21 02:03:55 +0530

    Add solution: 231-power-of-two

---
+++ 231-power-of-two/2026-02-21 02.03.55 - Accepted - runtime 1ms - memory 56.7MB.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    for(let i = 0 ; i < 2 ** 31 ; i ++){
+        if(n == 2 ** i){
+            return true ;
+        }
+        if(2 ** i > n){
+            return false ;
+        }
+    }
+};
