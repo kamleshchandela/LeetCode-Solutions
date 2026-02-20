4f0a747f75ae9ab53d0817447dcf669618b2ed5d
Kamlesh <kamleshchandela72@gmail.com>
2026-02-20 11:33:18 +0530

    Add solution: 3859-maximum-product-of-two-digits

---
+++ 3859-maximum-product-of-two-digits/2026-02-20 11.33.18 - Accepted - runtime 0ms - memory 8.8MB.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int a = n ;
+        int x = 0 ;
+        while(a>=1){
+            x++;
+            a = int(a / 10) ;
+        }
+        vector<int> arr(x) ; 
+        int count = 0 ; 
+        while(n >= 1){
+            int temp = n % 10 ;
+            arr[count] = temp ;
+            count++ ;
+            n = int(n / 10) ;
+        }
+        int max = -1 ;
+        for(int i = 0 ; i < arr.size() ; i ++){
+            for(int j = 0 ; j < arr.size() ; j ++){
+                if(max < (arr[i] * arr[j]) && i != j) {
+                    max = arr[i] * arr[j] ;
+                }
+            }
+        }
+        return max ;
+        }
+};
