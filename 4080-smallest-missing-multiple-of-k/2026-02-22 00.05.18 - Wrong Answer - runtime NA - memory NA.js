198273ae7bca5595aa10f08315b8817bc84a816b
Kamlesh <kamleshchandela72@gmail.com>
2026-02-22 00:05:18 +0530

    Add solution: 4080-smallest-missing-multiple-of-k

---
+++ 4080-smallest-missing-multiple-of-k/2026-02-22 00.05.18 - Wrong Answer - runtime NA - memory NA.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var missingMultiple = function(nums, k) {
+    if(nums.length == 1){
+        if(k == 1){
+            return 1 ;
+        }
+        
+        else{
+            return k ;
+        }
+        
+    }
+    for(let i = 1 ; i <= nums.length ; i++){
+        var check = true ;
+        for(let j = 0 ; j < nums.length ; j++){
+            if(nums[j] == k * i){
+                check = false ;
+                break ;
+            }
+        }
+        if(check){
+            return k * i ;
+        }
+    }
+};
