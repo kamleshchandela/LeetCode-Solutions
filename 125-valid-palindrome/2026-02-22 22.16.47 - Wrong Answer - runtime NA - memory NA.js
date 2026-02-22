cb7939018ab0b1030fdff998fbb9a9bd835e01e6
Kamlesh <kamleshchandela72@gmail.com>
2026-02-22 22:16:47 +0530

    Add solution: 125-valid-palindrome

---
+++ 125-valid-palindrome/2026-02-22 22.16.47 - Wrong Answer - runtime NA - memory NA.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    var str = "" ;
+    for(let i = 0 ; i < s.length ; i++ ){
+        if(s[i] == " " || s[i] == "," || s[i] == ":" || s[i] == "." || s[i] == "/" || s[i] == "!" || s[i] == "@" || s[i] == "#" || s[i] == "$" || s[i] == "." || 
+         s[i] == "_" || s[i] == `'` || s[i] == `{` || s[i] == `}` || s[i] == `[` || s[i] == `]` || s[i] == `"` || s[i] == `\\`  || s[i] == `|` || s[i] == "&" || s[i] == "*" || s[i] == "?"){
+            continue ;
+        }
+        else if(s.charCodeAt(i) >= 65 && s.charCodeAt(i) <= 90){
+            let a = s.charCodeAt(i) + 32 ;
+            str = str + String.fromCharCode(a) ;
+        }
+        else{
+            str = str + s[i] ;
+        }
+
+    }
+    if(str.length == 1) return true ;
+    for(let i = 0 ; i < parseInt(s.length / 2) ; i++){
+        if(str[i] != str[str.length - 1 - i]){
+            return false ;
+        }
+    }
+    return true ;
+};
