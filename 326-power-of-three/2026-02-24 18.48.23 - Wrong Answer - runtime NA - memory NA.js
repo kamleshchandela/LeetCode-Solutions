a55b03fead0d46f5f68f083e16ba67b386364b64
Kamlesh <kamleshchandela72@gmail.com>
2026-02-24 18:48:23 +0530

    Add solution: 326-power-of-three

---
+++ 326-power-of-three/2026-02-24 18.48.23 - Wrong Answer - runtime NA - memory NA.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n < 2) return false ;
+    for(let i = 2 ; i < n ; i ++){
+        if(n**(1/3) == i){
+            return true ;
+        }
+    }
+    return false;
+};
