65676ce52469800e7f357cc1657b52dbf454c6a2
Kamlesh <kamleshchandela72@gmail.com>
2026-02-24 17:55:32 +0530

    Add solution: 88-merge-sorted-array

---
+++ 88-merge-sorted-array/2026-02-24 17.55.32 - Accepted - runtime 0ms - memory 12.4MB.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = 0 ; i < n ; i++ ){
+            nums1[m + i] = nums2[i] ;
+        }
+        for(int i = 0; i < nums1.size() ; i ++){
+            int max = -2147483648 ; 
+            int index = 0 ; 
+            for(int j = 0 ; j < nums1.size() - i ; j++){
+                if(max < nums1[j]){
+                    max = nums1[j] ;
+                    index = j ; 
+                }
+            }
+            int temp = nums1[index] ;
+            nums1[index] = nums1[nums1.size() - 1 - i] ;
+            nums1[nums1.size() - 1 - i] = temp ;
+        }
+    }
+};
