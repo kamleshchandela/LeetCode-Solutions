0c79d81ce5230a417727e1cf3202cd00fbc3cdf5
Kamlesh <kamleshchandela72@gmail.com>
2026-03-03 16:14:48 +0530

    Add solution: 2292-counting-words-with-a-given-prefix

---
+++ 2292-counting-words-with-a-given-prefix/2026-03-03 16.14.48 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int count = 0 ;
+        for(int i = 0 ; i < words.size() ; i++){
+            int check = true ;
+            for(int j = 0 ; j < pref.size() ; j++){
+                if(words[i][j] != pref[j]){
+                    check = false ;
+                }
+            }
+            if(check){
+                count ++ ;
+            }
+        }
+        return count ;
+    }
+};
