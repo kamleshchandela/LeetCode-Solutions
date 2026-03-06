c1b1c6594e62db3be904fb236aed84b239568c0c
Kamlesh <kamleshchandela72@gmail.com>
2026-03-06 16:22:42 +0530

    Add solution: 189-rotate-array

---
+++ 189-rotate-array/2026-03-06 16.22.42 - Wrong Answer - runtime NA - memory NA.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    let arr = [] ;
+    let count = 0 ; 
+    for(let i = nums.length - k ; i < nums.length ; i++){
+        arr[count] = nums[i] ;
+        count ++ ;
+    }
+    for(let i = 0 ; i < nums.length - k ; i++){
+        arr[count] = nums[i] ;
+        count ++ ;
+    }
+    for(let i = 0 ; i < nums.length ; i++){
+        nums[i] = arr[i] ;
+    }
+    return nums ;
+};
