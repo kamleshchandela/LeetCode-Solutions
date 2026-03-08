3fe1e5792f189c1ab20dd49a2d40edad08122b50
Kamlesh <kamleshchandela72@gmail.com>
2026-03-08 21:56:49 +0530

    Add solution: 53-maximum-subarray

---
+++ 53-maximum-subarray/2026-03-08 21.56.49 - Accepted - runtime 0ms - memory 71.6MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = nums[0] ; 
+        int maxsum = nums[0] ;
+        for(int i = 1 ; i < nums.size() ; i++){
+            sum = sum + nums[i] ;
+            if(sum < nums[i]){
+                sum = nums[i] ;
+            }
+            if(sum >= maxsum){
+                maxsum = sum ;
+            }
+        }
+        return maxsum ; 
+    }
+};
