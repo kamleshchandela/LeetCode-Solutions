ac2d8165d7ed4d2aca213810adb3698d74f5c480
Kamlesh <kamleshchandela72@gmail.com>
2026-03-09 22:24:11 +0530

    Add solution: 151-reverse-words-in-a-string

---
+++ 151-reverse-words-in-a-string/2026-03-09 22.24.11 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string str = "" ;
+        for(int i = s.size() - 1 ; i >= 0 ; i--){
+            if(s[i] == ' '){
+                int j = i + 1 ;
+                
+                while(j < s.size() && s[j] != ' '){
+                    str = str + s[j] ;
+                    j ++ ;
+                }
+                if(i > 0 && s[i-1] != ' '){
+                    str = str + ' ' ;
+                }
+                
+            }
+            if(i == 0 && s[i] != ' '){
+                int j = i ;
+                while(s[j] != ' '){
+                    str = str + s[j] ;
+                    j ++ ;
+                }
+            }
+        }
+        string s1 = "" ;
+        if(str[0] == ' '){
+            
+            for(int i = 1 ; i < str.size() ; i++){
+                s1 = s1 + str[i] ;
+            }
+            return s1 ;
+        }
+        return str ;
+    }
+};
