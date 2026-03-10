2bde295a1af7efcbc5ce28f9ba4143c6f15b3682
Kamlesh <kamleshchandela72@gmail.com>
2026-03-10 23:19:24 +0530

    Add solution: 2278-find-three-consecutive-integers-that-sum-to-a-given-number

---
+++ 2278-find-three-consecutive-integers-that-sum-to-a-given-number/2026-03-10 23.19.24 - Accepted - runtime 0ms - memory 8.5MB.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<long long> sumOfThree(long long num) {
+        if(num % 3 == 0){
+            long long int a = num / 3 ;
+            return {(a - 1) , a , (a + 1)} ;
+        }
+        return {} ;
+    }
+};
