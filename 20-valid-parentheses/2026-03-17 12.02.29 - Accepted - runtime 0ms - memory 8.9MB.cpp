f14de6d4766647a7dcf694783b6abdc330040196
Kamlesh <kamleshchandela72@gmail.com>
2026-03-17 12:02:29 +0530

    Add solution: 20-valid-parentheses

---
+++ 20-valid-parentheses/2026-03-17 12.02.29 - Accepted - runtime 0ms - memory 8.9MB.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<char> arr ;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                arr.push_back(s[i]) ;
+            }
+            else{
+                if(arr.empty())return false ;
+                char temp = arr.back() ; 
+                arr.pop_back() ;
+                if(s[i] == ')' && temp != '('){
+                    return false ;
+                }
+                else if(s[i] == ']' && temp != '['){
+                    return false ;
+                }
+                else if(s[i] == '}' && temp != '{'){
+                    return false ;
+                }
+            }
+        }
+        return arr.size() == 0 ;
+    }
+};
