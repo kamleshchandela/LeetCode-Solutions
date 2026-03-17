c24c609384c6081e364af5d585db12aaf31cc252
Kamlesh <kamleshchandela72@gmail.com>
2026-03-17 11:42:16 +0530

    Add solution: 20-valid-parentheses

---
+++ 20-valid-parentheses/2026-03-17 11.42.16 - Accepted - runtime 1ms - memory 54.8MB.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let obj = {
+        "(" : ")" ,
+        "[" : "]" ,
+        "{" : "}" ,
+    } ;
+    let arr = [] ;
+    for(let i = 0 ; i < s.length ; i++){
+        if(s[i] == "(" || s[i] == "[" || s[i] == "{"){
+            arr.push(s[i]) ;
+        }
+        else{
+            if(s[i] == ")" && arr.pop() != "("){
+                return false ;
+            }
+            else if(s[i] == "]" && arr.pop() != "["){
+                return false ;
+            }
+            else if(s[i] == "}" && arr.pop() != "{"){
+                return false ;
+            }
+        }
+    }
+    return arr.length === 0 ;
+    return true ;
+    if(obj["("] != obj[")"]){
+        return false ;
+    }
+    else if (obj["["] != obj["]"]){
+        return false ;
+    }
+    else if (obj["{"] != obj["}"]){
+        return false ;
+    }
+    
+    return true ;
+};
