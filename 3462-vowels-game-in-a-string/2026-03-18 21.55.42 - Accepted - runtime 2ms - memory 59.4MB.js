7b285eb6dd8558aadcaa8d6541b65ab15dedad17
Kamlesh <kamleshchandela72@gmail.com>
2026-03-18 21:55:42 +0530

    Add solution: 3462-vowels-game-in-a-string

---
+++ 3462-vowels-game-in-a-string/2026-03-18 21.55.42 - Accepted - runtime 2ms - memory 59.4MB.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var doesAliceWin = function(s) {
+    for(let i = 0 ; i < s.length ; i++){
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
+            return true ;
+        }
+    }
+    return false ;
+};
