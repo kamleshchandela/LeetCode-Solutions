c7a634296b91e0518f72778c2e11c0a6a9673bc8
Kamlesh <kamleshchandela72@gmail.com>
2026-03-21 10:08:37 +0530

    Add solution: 1910-check-if-binary-string-has-at-most-one-segment-of-ones

---
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/2026-03-21 10.08.37 - Accepted - runtime 0ms - memory 53.7MB.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkOnesSegment = function(s) {
+    if(s.length == 1 || s.length == 2) return 1 ;
+    for(let i = 0 ; i < s.length - 1 ; i++){
+        if(s[i] == "0"){
+            for(let j = 0 ; j < s.length - 1 ; j++){
+                if(s[j] == "0" && s[j+1] != "0"){
+                    for(let k = 0 ; k < s.length - 1 ; k++){
+                        if (s[k] == "1" && s[k + 1] != "1"){
+                            return false ;
+                        }
+                    }
+                    return true ;
+                }
+            }
+            return true ;
+        }
+        else{
+            for(let j = 0 ; j < s.length - 1 ; j++){
+                if(s[j] == "1" && s[j+1] != "1"){
+                    for(let k = 0 ; k < s.length - 1 ; k++){
+                        if (s[k] == "0" && s[k + 1] != "0"){
+                            return false ;
+                        }
+                    }
+                    return true ;
+                }
+            }
+            return true ;
+        }
+        
+    }
+    return false ;
+};
