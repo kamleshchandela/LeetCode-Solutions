144726c067e68249843145db52fa4554b7ba1a31
Kamlesh <kamleshchandela72@gmail.com>
2026-03-23 03:46:21 +0530

    Add solution: 389-find-the-difference

---
+++ 389-find-the-difference/2026-03-23 03.46.21 - Wrong Answer - runtime NA - memory NA.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    for(let i = 0 ; i < t.length ; i++){
+        let check = true ;
+        for(let j = 0 ; j < s.length ; j++){
+            if(t[i] == s[j]){
+                check = false ;
+                break ;
+            }
+        }
+        if(check){
+            return t[i] ;
+        }
+    }
+    return t[t.length - 1] ;
+};
