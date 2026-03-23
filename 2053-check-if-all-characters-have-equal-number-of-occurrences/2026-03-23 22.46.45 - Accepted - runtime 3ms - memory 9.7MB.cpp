b88a7dd5c640567dfa302adb6d66a87189c617f9
Kamlesh <kamleshchandela72@gmail.com>
2026-03-23 22:46:45 +0530

    Add solution: 2053-check-if-all-characters-have-equal-number-of-occurrences

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2026-03-23 22.46.45 - Accepted - runtime 3ms - memory 9.7MB.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        map<char , int> obj ;
+        for(int i = 0 ; i < s.size() ; i++){
+            // if(obj[s[i]] == 0){
+            //     obj[s[i]] = 1 ;
+            // } 
+            // else{
+            //     obj[s[i]] += 1 ;
+            // }
+
+            // agar obj[s[i]] nahi he to uski value 0 he jo ham upar chech karva raheh to isiliye ham sidha obj[s[i]]++ likha sakte he or code ko aasan kar sakte he 
+
+            obj[s[i]]++ ;
+        }
+        int check = obj[s[0]] ; 
+        for(auto &i : obj){
+            if(i.second != check){
+                return false ;
+            }
+        }
+        return true ;
+    }
+};
