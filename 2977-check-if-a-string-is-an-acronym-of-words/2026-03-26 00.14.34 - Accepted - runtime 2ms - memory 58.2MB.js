a67d865766ee7e29a0d82f03c2ffd92b60b03d45
Kamlesh <kamleshchandela72@gmail.com>
2026-03-26 00:14:34 +0530

    Add solution: 2977-check-if-a-string-is-an-acronym-of-words

---
+++ 2977-check-if-a-string-is-an-acronym-of-words/2026-03-26 00.14.34 - Accepted - runtime 2ms - memory 58.2MB.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+var isAcronym = function(words, s) {
+    if(words.length == s.length){
+        for(let i = 0 ; i < words.length ; i++){
+            if(words[i][0]!=s[i]){
+                return false ;
+            }
+        }
+    }
+    else{
+        return false ;
+    }
+    return true ; 
+};
