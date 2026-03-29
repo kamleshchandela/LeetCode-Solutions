e62357e6b10fa23341d077583f874b8c784f4eab
Kamlesh <kamleshchandela72@gmail.com>
2026-03-29 22:35:46 +0530

    Add solution: 2999-check-if-strings-can-be-made-equal-with-operations-i

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/2026-03-29 22.35.46 - Wrong Answer - runtime NA - memory NA.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var canBeEqual = function(s1, s2) {
+    let count = 0 ; 
+    for(let i = 0 ; i < s1.length - 2 ; i++){
+        if(s1[i] != s2[i]){
+            if(s1[i+2] != s2[i+2]){
+            count ++ ;
+            }
+            else{
+                return false ;
+            }
+        }
+    }
+    return true ;
+};
