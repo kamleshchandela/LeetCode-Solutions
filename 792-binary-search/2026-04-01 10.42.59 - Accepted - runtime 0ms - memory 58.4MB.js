9f950b99acbe88e6c6115bb318943e711e9561b8
Kamlesh <kamleshchandela72@gmail.com>
2026-04-01 10:42:59 +0530

    Add solution: 792-binary-search

---
+++ 792-binary-search/2026-04-01 10.42.59 - Accepted - runtime 0ms - memory 58.4MB.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0 ; 
+    let r = nums.length - 1 ;
+    
+    while(l<=r){
+        let m = Math.floor((l + r)/2) ; 
+        if(nums[m] == target){
+            return m ;
+        }
+        else if (nums[m] < target){
+            l = m + 1 ;
+        }
+        else{
+            r = m - 1 ;
+        }
+    }
+    return -1 ;
+
+
+
+
+
+
+
+
+
+
+
+
+    // for(let i = 0 ; i < nums.length ; i++){
+    //     if(nums[i] == target){
+    //         return i ;
+    //     }
+    // }
+    // return -1 ;
+};
