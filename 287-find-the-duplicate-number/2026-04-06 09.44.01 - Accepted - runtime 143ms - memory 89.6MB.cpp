a558c9d398231dd912130bc1cf0f6fa3e4a881e2
Kamlesh <kamleshchandela72@gmail.com>
2026-04-06 09:44:01 +0530

    Add solution: 287-find-the-duplicate-number

---
+++ 287-find-the-duplicate-number/2026-04-06 09.44.01 - Accepted - runtime 143ms - memory 89.6MB.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // sort(nums.begin() , nums.end());
+        // for(int i = 0 ; i < nums.size() ; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return nums[i] ;
+        //     }
+        // }
+        map<int , int> map ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            map[nums[i]]++ ;
+            if(map[nums[i]] == 2){
+                return nums[i] ;
+            }
+        }
+        return -1 ;
+    }
+};
