47c3b84ce032da8a514ed16d7fafaf45f4268515
Kamlesh <kamleshchandela72@gmail.com>
2026-04-06 10:05:34 +0530

    Add solution: 136-single-number

---
+++ 136-single-number/2026-04-06 10.05.34 - Accepted - runtime 0ms - memory 20.7MB.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size() == 1){
+            return nums[0] ;
+        }
+        sort(nums.begin() , nums.end()) ;
+        for(int i = 0 ; i < nums.size() ; i = i + 2){
+            if((i == nums.size() - 1) && nums[i] != nums[i-1]){
+                return nums[i] ;
+            }
+            if(nums[i] != nums[i+1]){
+                return nums[i] ;
+            }
+        }
+        return -1 ;
+    }
+};
