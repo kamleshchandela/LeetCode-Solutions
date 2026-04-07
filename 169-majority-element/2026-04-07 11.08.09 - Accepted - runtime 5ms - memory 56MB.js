abc78d08f822c21914f0227bcfb2773a327ead00
Kamlesh <kamleshchandela72@gmail.com>
2026-04-07 11:08:09 +0530

    Add solution: 169-majority-element

---
+++ 169-majority-element/2026-04-07 11.08.09 - Accepted - runtime 5ms - memory 56MB.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+
+    let obj = {} ;
+    for(let i = 0 ; i < nums.length ; i++){
+        if(!obj[nums[i]]){
+            obj[nums[i]] = 1 ;
+        }
+        else{
+            obj[nums[i]] += 1 ;
+        }
+        if(obj[nums[i]] > nums.length / 2){
+            return nums[i] ;
+        }
+    }
+    
+    return -1 ;
+
+
+
+
+    // var obj = {} ;
+    // var count = 0 ;
+    // for(let i = 0 ; i < nums.length ; i++){
+    //     if(!obj[nums[i]]){
+    //         count ++ ;
+    //         obj[nums[i]] = 1 ;
+    //     }
+    //     else{
+    //         obj[nums[i]] = obj[nums[i]] + 1 ;
+    //     }
+    // }
+    // var max = -Infinity ;
+    // var ans = 0 ;
+    // for(let i in obj){
+    //     if(obj[i] > max){
+    //         if(obj[i] > max){
+    //                     max = obj[i] ;
+    //                     ans = Number(i) ;
+    //             }
+    //     }
+    // }
+    // return ans ;
+};
